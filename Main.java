8d54b92d6854f83d093e74441bd23703c55a0345
Hoa Doan <doanhd@miamioh.edu>
2022-01-10 07:56:01 -0800

    First commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    /**
+     * f(n) = n * (n - 1) * ... 1 = n * f(n - 1)
+     * write a function that calculates a factorial of a given number n.
+     *
+     * f(5)
+     * 5 * f(4)
+     *     4 * f(3)
+     *         3 * f(2)
+     *             2 * f(1)
+     *                 1 * f(0)
+     *                     0 * f(-1)
+     */
+    public static void main(String[] args) {
+        
+    }
+
+
+
+
+}
